70e9800b7bf8441779fa555ccb14cbdbeda40afa
AlexanderITWorker88888 <108433244+AlexanderITWorker88888@users.noreply.github.com>
2022-07-03 20:11:53 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,35 +1,14 @@
-/* Liczby do toto-lotka */
-
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
-int main() {
-    int a1=0,a2=0,a3=0,a4=0,a5=0,
-        liczba, numer, nowa;
-    srand(time(NULL));
-    numer = 0;
-    nowa = 0;
-    printf("Liczby do toto-lotka: ");
-    while ( numer<6 ) {
-        liczba = rand() % 49 + 1;
-        nowa=0;
-        if (liczba!=a1 && liczba!=a2 && liczba!=a3 && liczba!=a4 && liczba!=a5)
-            nowa = 1;
-        if (nowa) {
-            printf("   %d", liczba);
-            numer=numer+1;
-            if (numer==1) a1=liczba;
-            else
-            if (numer==2) a2=liczba;
-            else
-            if (numer==3) a3=liczba;
-            else
-            if (numer==4) a4=liczba;
-            else
-            if (numer==5) a5=liczba;
-        }
+int main()
+{
+    srand( time( NULL ) );
+    int tablica[ 5 ];
+      "Totolotek!" << endl << endl;
+    for( int i = 0; i < 6; i++ )
+    {
+        tablica[ i ] = rand() % 49 + 1;
+        cout << tablica[ i ] << " ";
     }
-    printf("\n");
-    return 0;
 }
